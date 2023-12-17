0fbd04e6b9df871d6c1b70f3804a6b8d19b0b545
hakim-layach-vinci <124344804+hakim-layach-vinci@users.noreply.github.com>
2023-12-17 19:37:34 +0100

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,119 @@
 const express = require('express');
+const {
+  getAllUsers, addOneUser, updateAccessByEmail, getStudentAccessInfo, nbreStudent,
+} = require('../models/users');
+const {
+  addAnnouce, getAllAnnouce,
+} = require('../models/annouce');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res) => {
-  res.json({ users: [{ name: 'e-baron' }] });
+// Get all users
+router.get('/', async (req, res) => {
+  console.log('on est arrivé dans la méthode GET/');
+  try {
+    const foundUsers = await getAllUsers();
+
+    if (!foundUsers) return res.sendStatus(404);
+
+    // Mettez à jour la liste pour inclure la propriété canAccessSite
+    const updatedUsers = foundUsers.map((user) => ({ ...user, canAccessSite: true }));
+
+    return res.json(updatedUsers);
+  } catch (error) {
+    console.error('Error getting users:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Mettre à jour canAccessSite côté serveur
+router.put('/updateAccess/:email', async (req, res) => {
+  console.log('on est arrivé dans la méthode /updateAccess/:email');
+  try {
+    const userEmail = req.params.email;
+
+    // Appeler la méthode de mise à jour de l'accès
+    const updatedUsers = await updateAccessByEmail(userEmail);
+
+    // Utilisez la variable mise à jour dans la réponse JSON
+    return res.json(updatedUsers);
+  } catch (error) {
+    console.error('Error updating access:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Add a user
+router.post('/', async (req, res) => {
+  console.log('on est arrivé dans la méthode POST/');
+  try {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).json({ error: 'Name and email are required fields' });
+    }
+
+    await addOneUser(name, email);
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error('Error adding user:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/pageRequiringAccess', async (req, res) => {
+  console.log('on est arrivé dans la méthode GET/pageRequiringAccess');
+
+  try {
+    const valeur = await getStudentAccessInfo(req.query.email);
+    return res.json(valeur);
+  } catch (error) {
+    console.error('Error getting acces:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/numberOfStudents', async (req, res) => {
+  try {
+    const nbre = await nbreStudent();
+    return res.json(nbre);
+  } catch (error) {
+    console.error('Error getting users:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/annouce', async (req, res) => {
+  console.log('on est arrivé dans la méthode POST/annouce');
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({ error: 'title and content are required fields' });
+    }
+
+    await addAnnouce(title, content);
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error('Error adding user:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/readAnnoucement', async (req, res) => {
+  console.log('on est arrivé dans la méthode GET/readAnnoucement');
+  try {
+    const foundAllAnnouce = await getAllAnnouce();
+
+    if (!foundAllAnnouce) return res.sendStatus(404);
+
+    // Mettez à jour la liste pour inclure la propriété canAccessSite
+    const last = foundAllAnnouce[foundAllAnnouce.length - 1];
+    return res.json(last);
+  } catch (error) {
+    console.error('Error getting users:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
